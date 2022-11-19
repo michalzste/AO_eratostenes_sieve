b2849ecece89e464a9dbdc283815dd54e05ea7f2
matCh00 <mateusz.chalik.g@gmail.com>
2022-11-19 19:17:02 +0100

    poprawa alokowania pamięci w createBitearrays oraz utworzenie funkcji sieveChunk

--- GPU/sieve.cu
+++ GPU/sieve.cu
@@ -1,22 +1,52 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <stdint.h>
+
+#define BYTE unsigned char
+
 
 /**
  * @brief Funkcja alokuje ciągły obszar pamięci na karcie graficznej o podanym rozmiarze
+ * optymalniej jest zaalokować jeden ciągły obszar niż tysiace mniejszych
  * 
- * @param items liczba elementów tablicy bitów, którą chcemy zaalokować
+ * @param elements liczba elementów tablicy bitów, którą chcemy zaalokować
  * @param bytesPerBitarray liczba bajtów na tablicę bitów
  * @param bitarrays  liczba tablic bitów, które chcemy zaalokować
  * 
- * @return unsigned char* 
+ * @return BYTE* 
  */
-inline unsigned char *createBitearrays(size_t items, size_t *bytesPerBitarray, size_t bitarrays) {
-  *bytesPerBitarray = items / 8;
-  unsigned char *bitarraysMem;
+inline BYTE *createBitearrays(size_t elements, size_t *bytesPerBitarray, size_t bitarrays) {
+
+  // dodajemy potencjalny bajt, gdy liczba bitów nie jest podzielna przez 8
+  *bytesPerBitarray = elements / 8 + ((elements % 8) > 1);  
+  BYTE *bitarraysMem;
   cudaMalloc(&bitarraysMem, *bytesPerBitarray * bitarrays);
   return bitarraysMem;
 }
 
+
+/**
+ * @brief Funkcja uruchamiana przez każdy wątek do szukania liczb pierwszych
+ * __global__ oznacza że funkcja zostanie uruchomiona na GPU
+ * 
+ * @param isPrimeArrays wskaźnik na tablicę bitów zaalokowaną funckją createBitearrays do której będą zapisywane informacje
+ * o liczbach pierwszych przez wiele wątków
+ * @param isPrimeBytes wielkość tablic bitowych używanych w poszczególnych wątkach (w bajtach)
+ * @param defaultPrimeCount makxymalna ilość liczb pierwszych w wątku 
+ * (przykładowa tablica ma wielkość 10, parzyste liczby nie mogą być pierwsze więc defaultPrimeCount będzie 5)
+ * @param primeCounts liczba znalezionych liczb pierwszych
+ * @param chunkCount liczba wątków/wywołań
+ * @return __global__ 
+ */
+__global__ void sieveChunk(BYTE *isPrimeArrays, size_t isPrimeBytes, uint64_t defaultPrimeCount, 
+                           uint64_t *primeCounts, uint64_t chunkCount) {
+
+  // index konkretnego wywołania
+  uint64_t index = blockIdx.x * blockDim.x + threadIdx.x;
+  
+  if (index >= chunkCount) return;
+}
+
 int main() {
-  printf("%d", 0);
+  printf("%d\n", 0);
   return 0;
 }
